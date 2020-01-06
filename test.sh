be23062f4d4dccd001610222e0c3c31d5f16342c
Andrzej Swatowski <andrzejswatowski@outlook.com>
2020-01-06 21:11:26 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -9,20 +9,14 @@ fi
 
 for g in $1/*.lat
 do
-    echo $g
     filename=$(basename -- "$g")
-    # directory=$(dirname -- "$1")
-    extension="${filename##*.}"
     filename="${filename%.*}"
 
-    $STACK run < $g 1> out.asm
-    nasm -f elf32 out.asm
-    #gcc -m32 -o run_out out.o lib/runtime.o
-    $BUILD
+    ./latc_x86 $g
     if [ -f $1/$filename.input ]; then
-        ./run_out < "$1/$filename.input" > out.output2
+        $1/$filename < $1/$filename.input > out.output2
     else
-        ./run_out > out.output2
+        $1/$filename > out.output2
     fi
     diff out.output2 $1/$filename.output
 done
